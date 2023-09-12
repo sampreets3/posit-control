32c3caea33d876ea6e947b7e8505d0b70588d735
Sampreet <sampreets3@gmail.com>
2023-09-12 19:39:39 +0200

    Added USB_DEVICE MiddleWare

--- firmware/Core/Inc/main.h
+++ firmware/Core/Inc/main.h
@@ -57,26 +57,26 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define M2_IN1_Pin 			GPIO_PIN_2
-#define M2_IN1_GPIO_Port 	GPIOA
-#define M2_IN2_Pin 			GPIO_PIN_3
-#define M2_IN2_GPIO_Port 	GPIOA
-#define M2_ENCA_Pin 		GPIO_PIN_4
-#define M2_ENCA_GPIO_Port 	GPIOA
-#define M2_ENCB_Pin 		GPIO_PIN_5
-#define M2_ENCB_GPIO_Port 	GPIOA
-#define BATT_STAT_Pin 		GPIO_PIN_1
+#define M2_IN1_Pin GPIO_PIN_2
+#define M2_IN1_GPIO_Port GPIOA
+#define M2_IN2_Pin GPIO_PIN_3
+#define M2_IN2_GPIO_Port GPIOA
+#define M2_ENCA_Pin GPIO_PIN_4
+#define M2_ENCA_GPIO_Port GPIOA
+#define M2_ENCB_Pin GPIO_PIN_5
+#define M2_ENCB_GPIO_Port GPIOA
+#define BATT_STAT_Pin GPIO_PIN_1
 #define BATT_STAT_GPIO_Port GPIOB
-#define STAT_LED_Pin 		GPIO_PIN_14
-#define STAT_LED_GPIO_Port 	GPIOB
-#define M1_ENCB_Pin 		GPIO_PIN_4
-#define M1_ENCB_GPIO_Port 	GPIOB
-#define M1_ENCA_Pin 		GPIO_PIN_5
-#define M1_ENCA_GPIO_Port 	GPIOB
-#define M1_IN1_Pin 			GPIO_PIN_6
-#define M1_IN1_GPIO_Port 	GPIOB
-#define M1_IN2_Pin 			GPIO_PIN_7
-#define M1_IN2_GPIO_Port 	GPIOB
+#define STAT_LED_Pin GPIO_PIN_14
+#define STAT_LED_GPIO_Port GPIOB
+#define M1_ENCB_Pin GPIO_PIN_4
+#define M1_ENCB_GPIO_Port GPIOB
+#define M1_ENCA_Pin GPIO_PIN_5
+#define M1_ENCA_GPIO_Port GPIOB
+#define M1_IN1_Pin GPIO_PIN_6
+#define M1_IN1_GPIO_Port GPIOB
+#define M1_IN2_Pin GPIO_PIN_7
+#define M1_IN2_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
