4b78e15b758612c1c1ade5ff88770d4492502913
Aniket SAHA <>
2024-04-04 18:39:08 +0200

    Replaced not working project with a working

--- motorController/Debug/motorControl/motor_driver.d
+++
@@ -1,5 +0,0 @@
-motorControl/motor_driver.o: ../motorControl/motor_driver.c \
- ../motorControl/motor_driver.h \
- /Users/aniket/STM32CubeIDE/workspace_1.14.0/PositMotorController/drv_low/drv_low.h
-../motorControl/motor_driver.h:
-/Users/aniket/STM32CubeIDE/workspace_1.14.0/PositMotorController/drv_low/drv_low.h:
